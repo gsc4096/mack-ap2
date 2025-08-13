45e3eb50717fa0b565305db7d98c14aac7bc6054
Gabriel Coelho <gsc4096@gmail.com>
2025-08-13 01:12:53 +0000

    fundação FCI

--- exemplo1.c
+++ exemplo1.c
@@ -8,6 +8,7 @@ int main() {
 	printf("Ciência da Computação\n");
 	printf("Algoritmos e Programação II\n");
 	printf("Turma: 02P\n");
+	printf("Fundação da FCI: 1970\n");
 	system("pause");
 	return 0;
 }
